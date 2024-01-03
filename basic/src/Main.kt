619ed4e957a0c63326874928437fdcf2d572d7d1
sambok23 <a24746440@gmail.com>
2024-01-03 14:21:57 +0900

    study init

---
+++ basic/src/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var i = 10
+    var name = "3boku"
+    var point = 3.3
+    //따로 타입 선언 안해줘도됨
+
+    var i2 : Int = 10
+    var name2 : String = "3boku"
+    var point2 : Double = 3.3
+    //타입 지정해줄때는 콜론 찍고 대문자로 시작하는 타입 적어주기
+    //자바에 있는 모든 타입 사용 가능 다만 대문자로 시작
+}
